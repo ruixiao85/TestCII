24ded6c011818e783da18d660e31a82917bda44d
CarolS LabWin10 <ruixiao85@gmail.com>
2019-05-05 01:41:51 -0400

    completed.

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import os
 import timeit
+import datetime
 from statistics import mean
 
-from Bio import SeqIO
+from Bio import SeqIO,motifs
 from Bio.Blast import NCBIWWW,NCBIXML
 from Bio.Seq import Seq
 from Bio.SeqIO.QualityIO import PairedFastaQualIterator
+
+def find_print_trim(_rec,_seq,_seq_name,_writer):
+	_len=len(_seq)
+	_motifs=motifs.create([_seq]) # potentially include ,_seq.complement(),_seq.reverse_complement()
+	_list=[]
+	for pos,seq in _motifs.instances.search(_rec.seq):
+		_writer.write('\t'.join([_rec.name,_seq_name,f'{pos}',f'{pos+_len}'])+'\n')
+		_list.append(pos)
+	if _list:
+		_list.reverse() # reverse in place
+		for pos in _list: # position move to the left or become smaller as we delete, so delete from the right side
+			_rec=_rec[:pos]+_rec[pos+_len:] # delete in place # TODO consider updating description length=?
+
+		return True,_rec # target sequence found
+	return False,rec # target sequence not found
 
 if __name__=='__main__':
 	fna,qual,fastq="test.fna","test.qual","test.fastq"
@@ -25,28 +41,36 @@ if __name__=='__main__':
 
 	fq=SeqIO.parse(fastq,"fastq")
 	c1,c2,c3,c4,c5,c6,c7=0,0,0,0,0,0,0
+	cor_len,cor_qual,primer_in,adaptor_in,pos_list=None,None,None,None,None # avoid temporary variables in the loop
 	now=timeit.timeit()
 	fb,cb=open(blast_result_file,'w'),0
 	fb.write('\t'.join(["query","subject","%id","alignment_length","mismatches","gap_openings",
 		"query_start","query_end","subject_start","subject_end","E_value","bit_score"])+'\n')
+	fft=open(filter_trim_file,'w') # fasta no header needed
+	fpa=open(primer_adaptor_file,'w')
+	fpa.write('\t'.join(["identifier","match","start","end"])+'\n')
 	for rec in fq:
 		c1+=1 # increment every entry # print(rec.seq)
-		if len(rec)>qc2: c2+=1 # increment if length met predefined criteria
+		cor_len=len(rec)>qc2
+		if cor_len: c2+=1 # increment if length met predefined criteria
 		scores=rec.letter_annotations["phred_quality"] # print(scores)
-		if mean(scores)>qc3: c3+=1 # increment if average quality score met predefined criteria
-		primer_in=primer in rec.seq
-		adaptor_in=adaptor in rec.seq
-		if primer_in: c4+=1
+		cor_qual=mean(scores)>qc3
+		if cor_qual: c3+=1 # increment if average quality score met predefined criteria
+		primer_in,rec=find_print_trim(rec,primer,"primer",fpa) # return whether primer found and trimmed sequence
+		adaptor_in,rec=find_print_trim(rec,adaptor,"adaptor",fpa) # return whether adaptor found and trimmed sequence
+		if primer_in:
+			c4+=1
 		if adaptor_in: c5+=1
-		if primer_in and adaptor_in:
-			c6+=1
+		if primer_in and adaptor_in: c6+=1
 
+		if cor_len and cor_qual:
+			SeqIO.write(rec,fft,"fasta")
 		blast_file=os.path.join(blast_file_folder,f'{blast_file_prefix}_{rec.name}.xml')
 		if not os.path.exists(blast_file): # qblast ncbi for xml format if not found
 			qblast=NCBIWWW.qblast("blastn","nt",rec.seq)
 			with open(blast_file,"w") as file:
 				file.write(qblast.read())
-			print(f'blast completed for {rec.name}')
+			print(f'blast completed for {rec.name} @ {datetime.datetime.now()}')
 			qblast.close()
 		if os.path.exists(blast_file): # if qblast and download successfully
 			qblast=open(blast_file)
@@ -59,21 +83,23 @@ if __name__=='__main__':
 							f'{hsp.expect:.1e}',f'{hsp.bits:.1e}']
 						fb.write("\t".join(fields)+"\n")
 				cb+=1 # count of sequences blasted and written down
-		# if c1>3: break # debug only, quickly check the first few entries
+		# if c1>4000: break
 	fb.close()
+	fft.close()
+	fpa.close()
 	print(f'Processed in {timeit.timeit()-now:.5f} seconds.')
 
 	print('The program should generate the following output:')
-	print(f'1.Total number of matching reads: {c1}.')
-	print(f'2.Number of reads greater than {qc2} {unit}: {c2}')
-	print(f'3.Number of reads with average quality score greater than {qc3}: {c3}')
-	print(f'4.Number of reads with primer sequences: {c4}')
-	print(f'5.Number of reads with adapter sequences: {c5}')
-	print(f'6.Number of reads with both primer and adapter sequences: {c6}')
+	print(f'1. Total number of matching reads: {c1}.')
+	print(f'2. Number of reads greater than {qc2} {unit}: {c2}')
+	print(f'3. Number of reads with average quality score greater than {qc3}: {c3}')
+	print(f'4. Number of reads with primer sequences: {c4}')
+	print(f'5. Number of reads with adapter sequences: {c5}')
+	print(f'6. Number of reads with both primer and adapter sequences: {c6}')
 
 	print('In addition, your program needs to generate the following files:')
-	print(f'1.Total number of reads blasted and written into the m8 format file [{blast_result_file}]: {cb} ({cb/c1:.1%}).')
-	print(f'2.Fasta file containing reads greater than {qc2} {unit}, average read quality scores greater than {qc3}, primers and adaptors trimmed (assuming "and" condition) [{filter_trim_file}].')
-	print(f'3.Tab de-limited text file containing the read identifiers along with the starting and end positions of the primer or adaptor sequences [{primer_adaptor_file}].')
+	print(f'1. Total number of reads blasted and written into the m8 format file "{blast_result_file}"": {cb} ({cb/c1:.1%}).')
+	print(f'2. Fasta file containing reads greater than {qc2} {unit}, average read quality scores greater than {qc3}, primers and adaptors trimmed (assuming and/&& condition) "{filter_trim_file}".')
+	print(f'3. Tab de-limited text file containing the read identifiers along with the starting and end positions of the primer or adaptor sequences "{primer_adaptor_file}".')
 
 
